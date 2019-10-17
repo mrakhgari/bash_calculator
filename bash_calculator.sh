a9d9b7bc14054868b251bce4a78305cf52d4ebd2
mrakhgari <mr.akhgari403@aut.ac.ir>
2019-10-17 20:44:15 +0330

    finished. added arg handler

--- bash_calculator.sh
+++ bash_calculator.sh
@@ -8,6 +8,13 @@
 
 # Functions 
 
+usage(){
+	echo "this script is a simple calcultor that calc sum(s), sub(S), multyply(m) and divid (d)"
+	echo "for sum return sum of all args"
+	echo "for sub holds the first input and subtracs the other inputs from it"
+	echo "for multply return multiply all of args"
+	echo "for devide return args[1]/args[2]"
+}
 
 display(){ # show message.
 	first=$1
@@ -16,7 +23,7 @@ display(){ # show message.
 }
 
 sum(){ # shows the sum of args.
-	sum=0
+	local sum=0
 	for i do
 		sum=$((sum+$i))
 	done
@@ -24,7 +31,7 @@ sum(){ # shows the sum of args.
 }
 
 sub(){ # Holds the first input and subtracts the other inputs from it
-	first=$1
+	local first=$1
 	shift 1
 	for i do
 		first=$((first -i))
@@ -33,7 +40,7 @@ sub(){ # Holds the first input and subtracts the other inputs from it
 }
 
 multply(){ # shows the result the multiplication of inputs
-	res=1
+	local res=1
 	for i do 
 		res=$((res*i))
 	done
@@ -44,17 +51,64 @@ divide(){ # divides the first input into the second input
 	display "divid" $(($1/$2))
 }
 
+
+############################################################################################
+
 # handler of args
 
-re='^[0-9]+$'
+# check for --help
+if [ $# -gt 0 ] && [ $1 == "--help" ] ; then
+	usage
+	exit 1
+fi
+
+# check size
+if [ $# -lt 3 ]; then 
+	echo "args is invalid, lentgh is lower than 3";exit 1
+fi
+
+re='^[0-9]+$' # check command enterd.
 if  [[ $1 =~ $re ]] ; then
    echo "enter command " >&2; exit 1
 fi
 
-shift 1
-echo $@
-sub $@
-sum  $@
-multply $@
-divide $@ 
 
+com=$1
+shift 1 #remove command
+
+#for i; do
+#	if [ $i == "s" ] ; then
+#		$cansum="true"
+#	elif [ $i == "S" ];then 
+#		cansub="true"
+#	elif [ $i == "m" ]; then
+#		canmol="true"
+#	elif [ $i == "d" ]; then 
+#		candiv="true"
+#	elif [[ $i =~ $re ]]; then
+#		break
+#	else 
+#		usage
+#		exit 1
+#	fi
+#done
+
+#while getopts "$com" opt; do 
+#	case $opt in
+#		s) echo :1 ;sum $@;;
+#		S) echo :2 ;sub $@ ;;
+#		m) echo :3 ;multply $@;;
+#		d) echo :4 ;divide $@;;
+#	esac
+#done
+#echo $cansum is 
+#if [ $cansum == "true" ]; then
+#	sub $@
+#fi
+case $com in
+	s) sum $@;;
+	S) sub $@;;
+	m) multply $@;;
+	d) divide $@;;
+	*) usage ; exit 1;;
+esac
